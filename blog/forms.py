c6a6f0ecad7ea4553c6ddea051725a1a39580fb2
jim109 <josma109@gmail.com>
2015-12-16 10:06:47 -0500

    first commit blog Django

---
+++ blog/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import Post, Comment
+from django.core.context_processors import csrf
+
+class PostForm(forms.ModelForm):
+
+	class Meta:
+		model = Post
+		fields = ('title', 'text',)
+
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model = Comment
+        fields = ('author', 'text',)
